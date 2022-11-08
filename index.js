b6a513e462850a116dde34117c1a4968e2359238
shihab0 <ataharshihab1@gmail.com>
2022-11-08 09:25:49 +0600

    data limit set

--- index.js
+++ index.js
@@ -2,12 +2,47 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
+//Mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ueibnfi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function dbConnect() {
+  try {
+    client.connect();
+    console.log("Mongodb Connected");
+  } finally {
+  }
+}
+dbConnect().catch((err) => console.log(err));
+
+const serviceCollection = client.db("healthAid").collection("services");
+
+//CURD OPERATION
+app.get("/services", async (req, res) => {
+  try {
+    const query = {};
+    const cursor = serviceCollection.find(query).limit(3);
+    const services = await cursor.toArray();
+    res.send(services);
+  } catch {
+    console.error();
+  }
+});
+
 //Test
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Server is running");
 });
 
